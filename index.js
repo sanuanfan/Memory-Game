34e62f13c84edb83c1ed7d6402d398ec6c6a4dca
sanuanfan <sanuanfan8535@gmail.com>
2024-07-29 22:40:37 +0530

    started

---
+++ index.js
@@ -0,0 +1,7 @@
+const settingsdiv = document.querySelector(".setting");
+const btn4 = document.querySelector("#4");
+const btn6 = document.querySelector("#6");
+const startbtn = document.querySelector(".startbtn");
+const gridcontainer = document.querySelector(".gridcontainer");
+const maingrid = document.querySelector(".maingrid");
+
